5e5d54cd7337a3d9544b58217de80ebcdf3f483f
Cosmic <130684643+C0msic@users.noreply.github.com>
2024-05-21 16:27:35 -0700

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import Button from "../components/button";
-import PrivacyPolicy from "../components/PrivacyPolicy"
 
 function Home() {
   return (
@@ -14,7 +13,6 @@ function Home() {
           Start playing!
         </Button>
       </Link>
-      <PrivacyPolicy/>
     </>
   );
 }
