47b700954bbe5261ff6fa1f508a0b924e315ce91
Cosmic <130684643+C0msic@users.noreply.github.com>
2024-05-21 15:22:40 -0700

    a

---
+++ src/components/PrivacyPolicy.tsx
@@ -0,0 +1,12 @@
+import { button } from "../components/button"
+
+
+function PrivacyPolicy() { 
+<div className="h-15 w-10">
+    <p>your gay</p>
+
+<Button/>
+</div>
+}
+
+export default PrivacyPolicy
